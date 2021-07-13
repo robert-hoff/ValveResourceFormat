11cd77cd0e2994f97b9215e1da6cf9f380f05904
Hoff Inc <hoff.rj@gmail.com>
2021-07-13 04:00:13 +0100

    fixes

--- TestMyVRF/Program.cs
+++ TestMyVRF/Program.cs
@@ -2,11 +2,9 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using SteamDatabase.ValvePak;
+using MyValveResourceFormat;
 using MyValveResourceFormat.Blocks;
 using MyValveResourceFormat.ResourceTypes;
-
-
-
 
 namespace TestMyVRF
 {
