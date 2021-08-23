b4ad2824b047e9d8586de7878d5e9fc2a6b19c21
Hoff Inc <hoff.rj@gmail.com>
2021-08-23 14:29:06 +0100

    full file summary

--- MyShaderAnalysis/StaticAnalysis3.cs
+++ MyShaderAnalysis/StaticAnalysis3.cs
@@ -39,12 +39,13 @@ namespace MyShaderAnalysis
 
         static void PrintSingleFileMainParamsAndConstaints(string outputFilenamepath = null, bool writeFile = false)
         {
-            // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/multiblend_pcgl_30_features.vcs";
             // string filenamepath = $"{DOTA_CORE_PCGL_SOURCE}/cs_compress_dxt5_pcgl_30_features.vcs";
-            string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/hero_pcgl_30_features.vcs";
+            // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/hero_pcgl_30_features.vcs";
             // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/hero_pcgl_30_ps.vcs";
+            // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/multiblend_pcgl_30_features.vcs";
             // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/multiblend_pcgl_30_vs.vcs"; // doesn't have any mipmaps
             // string filenamepath = $"{DOTA_GAME_PCGL_SOURCE}/multiblend_pcgl_30_ps.vcs";
+            string filenamepath = $"{DOTA_CORE_PCGL_SOURCE}/generic_light_pcgl_30_features.vcs";
             if (outputFilenamepath != null && writeFile)
             {
                 output.SetOutputFile(outputFilenamepath);
@@ -64,7 +65,11 @@ namespace MyShaderAnalysis
             PrintStaticConstraints(shaderFile);
             PrintDynamicConfigurations(shaderFile);
             PrintDynamicConstraints(shaderFile);
-
+            PrintParameters(shaderFile);
+            PrintMipmapBlocks(shaderFile);
+            PrintBufferBlocks(shaderFile);
+            PrintVertexSymbolBuffers(shaderFile);
+            PrintZFrames(shaderFile);
         }
 
 
@@ -89,7 +94,7 @@ namespace MyShaderAnalysis
                 output.WriteLine($"{ftHeader.editorIDs[i].Item1}    {ftHeader.editorIDs[i].Item2}");
             }
             output.WriteLine($"{ftHeader.editorIDs[^1].Item1}    // Editor ref. ID{ftHeader.editorIDs.Count - 1} " +
-                $"- this ID is seen across archives for vcs files sharing minor-version = {shaderFile.possibleMinorVersion}");
+                $"- this ID is shared across archives for vcs files with minor-version = {shaderFile.possibleMinorVersion}");
             output.BreakLine();
             if (ftHeader.mainParams.Count == 0)
             {
@@ -126,7 +131,7 @@ namespace MyShaderAnalysis
             output.WriteLine("Editor/Shader compiler stack");
             output.WriteLine($"{shaderFile.vspsHeader.fileID0}    // Editor ref. ID0 (produces this file)");
             output.WriteLine($"{shaderFile.vspsHeader.fileID1}    // Editor ref. ID1 " +
-                $"- this ID is seen across archives for vcs files sharing minor-version = {shaderFile.possibleMinorVersion}");
+                $"- this ID is shared across archives for vcs files with minor-version = {shaderFile.possibleMinorVersion}");
             output.BreakLine();
         }
 
@@ -315,6 +320,236 @@ namespace MyShaderAnalysis
 
 
 
+        static void PrintParameters(ShaderFile shaderFile)
+        {
+            output.DefineHeaders(new string[] { "index", "name", "arg2", "arg3", "arg4" });
+            foreach (var item in shaderFile.sfBlocks)
+            {
+                output.AddTabulatedRow(new string[] {$"[{item.blockIndex,2}]", $"{item.name0}", $"{item.arg2}",
+                    $"{item.arg3}", $"{item.arg4,2}"});
+            }
+            output.printTabulatedValues();
+            output.BreakLine();
+
+            // parameters
+            output.WriteLine("PARAMETERS");
+            output.DefineHeaders(new string[] { "index", "name", "type", "res", "ld0", "arg0", "arg1", "arg2", "arg3", "arg4", "arg5", "dyn-exp" });
+            foreach (var param in shaderFile.paramBlocks)
+            {
+                string nameCondensed = param.name0;
+                if (param.name1.Length > 0)
+                {
+                    nameCondensed += $" | {param.name1}";
+                }
+                if (param.name2.Length > 0)
+                {
+                    nameCondensed += $" | {param.name2}(2)";
+                }
+
+                int indexPad = shaderFile.paramBlocks.Count > 100 ? 3 : 2;
+                string dynExpstring = new VfxEval().ParseExpression(param.dynExp);
+
+                output.AddTabulatedRow(new string[] {$"[{(""+param.blockIndex).PadLeft(indexPad)}]", $"{nameCondensed}", $"{param.type}",
+                    $"{param.res0}", $"{param.lead0}", $"{BlankMOne(param.arg0),2}", $"{param.arg1,2}", $"{param.arg2}",
+                    $"{Pow2Rep(param.arg3),4}", $"{param.arg4,2}", $"{BlankMOne(param.arg5),2}", $"{dynExpstring}"});
+
+            }
+            output.printTabulatedValues();
+            output.BreakLine();
+
+
+
+
+
+            output.WriteLine("PARAMETERS - Default values and ranges (as well as command and fileref arguments if these exist)");
+            output.WriteLine("(- indicates -infinity, + indicates +infinity)");
+            output.DefineHeaders(new string[] { "index", "name", "ints0", "ints1", "ints2", "floats0", "floats1", "floats2",
+                "ints3", "ints4", "command", "fileref"});
+            foreach (var param in shaderFile.paramBlocks)
+            {
+                int indexPad = shaderFile.paramBlocks.Count > 100 ? 3 : 2;
+                string fileref = param.fileref;
+                int[] r0 = param.ranges0;
+                int[] r1 = param.ranges1;
+                int[] r2 = param.ranges2;
+                float[] r3 = param.ranges3;
+                float[] r4 = param.ranges4;
+                float[] r5 = param.ranges5;
+                int[] r6 = param.ranges6;
+                int[] r7 = param.ranges7;
+                string c0 = param.command0;
+                string c1 = param.command1;
+                if (c1.Length > 0)
+                {
+                    c0 += $" | {c1}";
+                }
+
+                output.AddTabulatedRow(new string[] { $"[{("" + param.blockIndex).PadLeft(indexPad)}]"
+                    ,$"{param.name0}", $"{comb(r0)}", $"{comb(r1)}", $"{comb(r2)}", $"{comb(r3)}", $"{comb(r4)}", $"{comb(r5)}",
+                    $"{comb(r6)}", $"{comb(r7)}", $"{c0}", $"{param.fileref}"});
+            }
+            output.printTabulatedValues();
+            output.BreakLine();
+        }
+
+
+        static void PrintMipmapBlocks(ShaderFile shaderFile)
+        {
+            output.WriteLine($"MIPMAP BLOCKS({shaderFile.mipmapBlocks.Count})");
+            if (shaderFile.mipmapBlocks.Count > 0)
+            {
+                output.DefineHeaders(new string[] { "index", "name", "arg0", "arg1", "arg2", "arg3", "arg4", "arg5" });
+            } else
+            {
+                output.DefineHeaders(Array.Empty<string>());
+                output.WriteLine("[none]");
+            }
+            foreach (var mipmap in shaderFile.mipmapBlocks)
+            {
+                output.AddTabulatedRow(new string[] { $"[{mipmap.blockIndex,2}]", $"{mipmap.name}",
+                    $"{ShaderDataReader.BytesToString(mipmap.arg0),-14}", $"{mipmap.arg1,2}", $"{BlankMOne(mipmap.arg2),2}",
+                    $"{BlankMOne(mipmap.arg3),2}", $"{BlankMOne(mipmap.arg4),2}", $"{mipmap.arg5,2}" });
+            }
+            output.printTabulatedValues();
+            output.BreakLine();
+        }
+
+
+        static void PrintBufferBlocks(ShaderFile shaderFile)
+        {
+            foreach (var bufferBlock in shaderFile.bufferBlocks)
+            {
+                output.WriteLine($"BUFFER-BLOCK[{bufferBlock.blockIndex}]");
+                output.WriteLine($"{bufferBlock.name} size={bufferBlock.bufferSize} param-count={bufferBlock.paramCount}" +
+                    $" arg0={bufferBlock.arg0} crc32={bufferBlock.blockCrc:x08}");
+                output.DefineHeaders(new string[] { "       ", "name", "offset", "vertex-size", "attrib-count", "data-count" });
+                foreach (var bufferParams in bufferBlock.bufferParams)
+                {
+                    string name = bufferParams.Item1;
+                    int bOffset = bufferParams.Item2;
+                    int nrVertices = bufferParams.Item3;
+                    int nrAttribs = bufferParams.Item4;
+                    int length = bufferParams.Item5;
+                    output.AddTabulatedRow(new string[] { "", $"{name}", $"{bOffset,3}", $"{nrVertices,3}", $"{nrAttribs,3}", $"{length,3}" });
+
+                }
+                output.printTabulatedValues();
+                output.BreakLine();
+            }
+        }
+
+
+        static void PrintVertexSymbolBuffers(ShaderFile shaderFile)
+        {
+            output.WriteLine($"VERTEX-BUFFER-SYMBOLS({shaderFile.symbolBlocks.Count})");
+            if (shaderFile.symbolBlocks.Count == 0)
+            {
+                output.WriteLine("[none]");
+                output.BreakLine();
+                return;
+            }
+            // find best padding
+            int namePad = 0;
+            int typePad = 0;
+            int optionPad = 0;
+            foreach (var symbolBlock in shaderFile.symbolBlocks)
+            {
+                foreach (var symbolsDef in symbolBlock.symbolsDefinition)
+                {
+                    if (symbolsDef.Item1.Length > namePad)
+                    {
+                        namePad = symbolsDef.Item1.Length;
+                    }
+                    if (symbolsDef.Item2.Length > typePad)
+                    {
+                        typePad = symbolsDef.Item2.Length;
+                    }
+                    if (symbolsDef.Item3.Length > optionPad)
+                    {
+                        optionPad = symbolsDef.Item3.Length;
+                    }
+                }
+            }
+            foreach (var symbolBlock in shaderFile.symbolBlocks)
+            {
+                output.WriteLine($"VERTEX-SYMBOLS[{symbolBlock.blockIndex}] definitions={symbolBlock.symbolsCount}");
+                output.DefineHeaders(new string[] { "       ", "name".PadRight(namePad), "type".PadRight(typePad),
+                    $"option".PadRight(optionPad), "semantic-index" });
+                foreach (var symbolsDef in symbolBlock.symbolsDefinition)
+                {
+                    string name = symbolsDef.Item1;
+                    string type = symbolsDef.Item2;
+                    string option = symbolsDef.Item3;
+                    int semanticIndex = symbolsDef.Item4;
+                    output.AddTabulatedRow(new string[] { "", $"{name}", $"{type}", $"{option}", $"{semanticIndex,2}" });
+                }
+                output.printTabulatedValues();
+                output.BreakLine();
+            }
+            output.BreakLine();
+        }
+
+
+        static void PrintZFrames(ShaderFile shaderFile)
+        {
+            string zframesHeader = $"ZFRAMES({shaderFile.GetZFrameCount()})";
+            output.WriteLine(zframesHeader);
+            if (shaderFile.GetZFrameCount()==0)
+            {
+                string infoText = "";
+                if (shaderFile.vcsFileType == VcsFileType.Features)
+                {
+                    infoText = "(features files in general don't contain zframes)";
+                }
+                output.WriteLine($"[none defined] {infoText}");
+                output.BreakLine();
+                return;
+            }
+
+            FileTokens fileTokens = new FileTokens(shaderFile.filenamepath);
+            // print the config headers every 100 frames
+            int zframeCount = 0;
+            // print the zframes
+            string zFrameBaseDir = $"/vcs-all/{GetCoreOrDotaString(shaderFile.filenamepath)}/zsource/";
+            // prepare the lookup to determine configuration state
+            CompatRulesGeneration configGen = new(shaderFile);
+            output.WriteLine(new string('-', zframesHeader.Length));
+
+            // collect names in the order they appear
+            List<string> sfNames = new();
+            List<string> abbreviations = new();
+
+            foreach (var sfBlock in shaderFile.sfBlocks)
+            {
+                string sfShortName = ShortenShaderParam(sfBlock.name0).ToLower();
+                abbreviations.Add($"{sfBlock.name0}({sfShortName})");
+                sfNames.Add(sfShortName);
+            }
+
+            string[] breakabbreviations = CombineValuesBreakString(abbreviations.ToArray(), 120);
+            foreach (string abbr in breakabbreviations)
+            {
+                output.WriteLine(abbr.Replace("(", "<span style='color: blue'>(").Replace(")", "</span>)"));
+            }
+            output.BreakLine();
+
+            string configHeader = CombineStringsSpaceSep(sfNames.ToArray(), 6);
+            configHeader = $"{new string(' ', 14)}{configHeader}";
+            foreach (var item in shaderFile.zframesLookup)
+            {
+                if (zframeCount % 100 == 0)
+                {
+                    output.WriteLine($"{configHeader}");
+                }
+                int[] configState = configGen.GetConfigState(item.Key);
+                string zframeLink = fileTokens.GetBestZframesLink(item.Key);
+                output.WriteLine($"{zframeLink} {CombineIntsSpaceSep(configState, 6)}");
+                zframeCount++;
+            }
+        }
+
+
+
 
 
         static void PrintSingleFileAnalysisZFrames(string outputFilenamepath = null, bool writeFile = false)
@@ -335,7 +570,6 @@ namespace MyShaderAnalysis
             }
             output.printTabulatedValues();
             output.BreakLine();
-
 
             FileTokens fileTokens = new FileTokens(shaderFile.filenamepath);
             // print the config headers every 100 frames
@@ -381,6 +615,10 @@ namespace MyShaderAnalysis
                 zframeCount++;
             }
         }
+
+
+
+
 
 
 
@@ -442,15 +680,12 @@ namespace MyShaderAnalysis
                 output.BreakLine();
             }
 
-
             output.WriteLine($"VERTEX-BUFFER-SYMBOLS({shaderFile.symbolBlocks.Count})");
             if (shaderFile.symbolBlocks.Count == 0)
             {
                 output.WriteLine("[none]");
             }
-
-
-            // find best passing
+            // find best padding
             int namePad = 0;
             int typePad = 0;
             int optionPad = 0;
