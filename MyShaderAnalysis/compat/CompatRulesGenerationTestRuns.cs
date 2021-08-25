402c704fe41a10fcbe88d870051db6cb70c92b5e
Hoff Inc <hoff.rj@gmail.com>
2021-08-25 04:34:01 +0100

    fix

--- MyShaderAnalysis/compat/CompatRulesGenerationTestRuns.cs
+++ MyShaderAnalysis/compat/CompatRulesGenerationTestRuns.cs
@@ -32,7 +32,7 @@ namespace MyShaderAnalysis.compat
             // string vcsFilenamepath = @$"{PCGL_DIR_NOT_CORE}\water_dota_pcgl_30_ps.vcs";
 
             ShaderFile shaderFile = new ReadShaderFile(vcsFilenamepath).GetShaderFile();
-            SfBlockConfigurationMapping configGen = new(shaderFile);
+            ConfigMappingSParams configGen = new(shaderFile);
             // zframeGen.ShowOffsetAndLayersArrays();
 
 
