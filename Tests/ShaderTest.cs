551a8fc652844ed2aeecaa3c1dfaec4099dabcc2
Hoff Inc <hoff.rj@gmail.com>
2021-08-26 14:37:13 +0100

    Updated GetVcsSourceType method - using String.split

--- Tests/ShaderTest.cs
+++ Tests/ShaderTest.cs
@@ -32,34 +32,24 @@ namespace Tests
         [Test]
         public void TestVcsSourceTypes()
         {
-            var filenamepath1 = "/sourcedir/bloom_dota_mobile_gles_30_ps.vcs";
-            Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath1));
-            var filenamepath2 = "/sourcedir/bloom_dota_ios_vulkan_40_ps.vcs";
-            Assert.AreEqual(VcsSourceType.IosVulkan, GetVcsSourceType(filenamepath2));
-            //var filenamepath3 = "/sourcedir/flow_map_preview_android_vulkan_40_vs.vcs";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath3));
-            //var filenamepath4 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath4));
-            //var filenamepath5 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath5));
-            //var filenamepath6 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath6));
-            //var filenamepath7 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath7));
-            //var filenamepath8 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath8));
-            //var filenamepath9 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath9));
-            //var filenamepath10 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath10));
-            //var filenamepath11 = "/sourcedir/";
-            //Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath11));
-
-
-
+            var filenamepath1 = "/sourcedir/multiblend_pcgl_40_ps.vcs";
+            Assert.AreEqual(VcsSourceType.Glsl, GetVcsSourceType(filenamepath1));
+            var filenamepath2 = "/sourcedir/solid_sky_pcgl_30_features.vcs";
+            Assert.AreEqual(VcsSourceType.Glsl, GetVcsSourceType(filenamepath2));
+            var filenamepath3 = "/sourcedir/copytexture_pc_30_ps.vcs";
+            Assert.AreEqual(VcsSourceType.DXIL, GetVcsSourceType(filenamepath3));
+            var filenamepath4 = "/sourcedir/copytexture_pc_40_ps.vcs";
+            Assert.AreEqual(VcsSourceType.DXBC, GetVcsSourceType(filenamepath4));
+            var filenamepath5 = "/sourcedir/deferred_shading_pc_41_ps.vcs";
+            Assert.AreEqual(VcsSourceType.DXBC, GetVcsSourceType(filenamepath5));
+            var filenamepath6 = "/sourcedir/bloom_dota_mobile_gles_30_ps.vcs";
+            Assert.AreEqual(VcsSourceType.MobileGles, GetVcsSourceType(filenamepath6));
+            var filenamepath7 = "/sourcedir/cs_volumetric_fog_vulkan_50_cs.vcs";
+            Assert.AreEqual(VcsSourceType.Vulkan, GetVcsSourceType(filenamepath7));
+            var filenamepath8 = "/sourcedir/bloom_dota_ios_vulkan_40_ps.vcs";
+            Assert.AreEqual(VcsSourceType.IosVulkan, GetVcsSourceType(filenamepath8));
+            var filenamepath9 = "/sourcedir/flow_map_preview_android_vulkan_40_vs.vcs";
+            Assert.AreEqual(VcsSourceType.AndroidVulkan, GetVcsSourceType(filenamepath9));
         }
-
-
-
     }
 }
