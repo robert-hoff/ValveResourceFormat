e3a15bc0e80dbd1eba5ec07c2884ba122624aa77
Hoff Inc <hoff.rj@gmail.com>
2022-04-02 17:38:46 +0100

    c++ test

--- TestVRFDev/ProgramEntry.cs
+++ TestVRFDev/ProgramEntry.cs
@@ -1,16 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyCSharpLib;
 
 namespace TestVRFDev {
+
+    // this can only be used together with the 'sizeof' method if calling method is declared as 'unsafe'
+    // (which must also be specifed as a build option)
+    struct myGoodStruct
+    {
+        int i;
+        float f;
+    }
+
     class ProgramEntry {
+        public class ToDebugWriter : StringWriter
+        {
+            public override void WriteLine(string str0)
+            {
+                Debug.WriteLine(str0);
+            }
+            public override void Write(string str0)
+            {
+                Debug.Write(str0);
+            }
+        }
 
-        static void Main() {
 
-            // ReadingVmdlFile2.RunTrials();
 
+        unsafe static void Main() {
+
+            Console.SetOut(new ToDebugWriter());
+
+
+            // Console.WriteLine($"hello");
+            Console.WriteLine($"{Calculate.Add(2, 2)}");
+
+            var i = sizeof(myGoodStruct);
+            Console.WriteLine($"{i}");
 
 
         }
