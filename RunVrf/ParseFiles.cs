4e5f0016c297f57f9835ce041bda33ec5ddace7a
Hoff Inc <hoff.rj@gmail.com>
2023-02-18 01:45:04 +0000

    printing to html

--- RunVrf/ParseFiles.cs
+++ RunVrf/ParseFiles.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RunVrf.UtilHelpers.LSystemsMG.Util.External;
 using ValveResourceFormat.CompiledShader;
 
 namespace RunVrf
@@ -13,12 +14,14 @@ namespace RunVrf
 
         public static void RunTrials()
         {
-            Go1();
+            // Trials1();
+            PrintZframeToHtml();
+            // PrintZframeByteVersion();
         }
 
 
 
-        public static void Go1()
+        public static void Trials1()
         {
             ZFrameFile zFrameFile = GetZFrameFile();
             //Debug.WriteLine($"{zFrameFile.ZframeParams[0].HasStaticVal}");
@@ -28,24 +31,52 @@ namespace RunVrf
             //Debug.WriteLine($"{zFrameFile.ZframeParams[0].HeaderOperator}");
 
 
-            foreach (var zFrameParams in zFrameFile.ZframeParams)
-            {
-                Debug.WriteLine($"{zFrameParams}");
-                Debug.WriteLine($"{zFrameParams.LinkedParameterIndex}");
-            }
+            //foreach (var zFrameParams in zFrameFile.ZframeParams)
+            //{
+            //    Debug.WriteLine($"{zFrameParams}");
+            //    Debug.WriteLine($"{zFrameParams.LinkedParameterIndex}");
+            //}
+
 
         }
 
+
+        public static void PrintZframeToHtml()
+        {
+            string htmlTitle = $"Z[0x{zFrameIndex:x}]";
+            string htmlHeader = $"{Path.GetFileName(inputFile)}  Z[0x{zFrameIndex:x}]";
+            FileWriter fw = new FileWriter(OUTPUT_FILENAME, true, true, htmlTitle, htmlHeader);
+            ZFrameFile zFrameFile = GetZFrameFile();
+            _ = new PrintZFrameSummary(shaderFile, zFrameFile, (x) => { fw.Write(x); }, showRichTextBoxLinks: true);
+            fw.CloseStreamWriter();
+        }
+
+        public static void PrintZframeByteVersion()
+        {
+            string htmlTitle = $"Z[0x{zFrameIndex:x}]";
+            string htmlHeader = $"{Path.GetFileName(inputFile)}  Z[0x{zFrameIndex:x}]";
+            FileWriter fw = new FileWriter(OUTPUT_FILENAME_BYTES, true, true, htmlTitle, htmlHeader);
+            ZFrameFile zFrameFile = GetZFrameFile();
+            zFrameFile.PrintByteDetail((x) => { fw.Write(x); });
+            fw.CloseStreamWriter();
+
+        }
+
+
+        static string OUTPUT_FILENAME = "output2.html";
+        static string OUTPUT_FILENAME_BYTES = "output2-bytes.html";
+        static ShaderFile shaderFile;
 
 
         static string DOTA_PCGL_V64_GAME = @"X:\dota-2-VRF-exports\dota2-export-shaders-pcgl\shaders\vfx\";
         // static string inputFile = $"{DOTA_PCGL_V64_GAME}cables_pcgl_40_vs.vcs"; static int zFrameIndex = 0;
         // static string inputFile = $"{DOTA_PCGL_V64_GAME}hero_pcgl_40_vs.vcs"; static int zFrameIndex = 0x17fa2;
-        static string inputFile = $"{DOTA_PCGL_V64_GAME}hero_pcgl_40_ps.vcs"; static int zFrameIndex = 0x18cbfb1;
+        // static string inputFile = $"{DOTA_PCGL_V64_GAME}hero_pcgl_40_ps.vcs"; static int zFrameIndex = 0x18cbfb1;
+        static string inputFile = $"{DOTA_PCGL_V64_GAME}spring_meteor_pcgl_30_vs.vcs"; static int zFrameIndex = 0;
 
         public static ZFrameFile GetZFrameFile()
         {
-            ShaderFile shaderFile = new ShaderFile();
+            shaderFile = new ShaderFile();
             shaderFile.Read(inputFile);
             return shaderFile.GetZFrameFile(zFrameIndex);
         }
---
+++ RunVrf/UtilHelpers/FileWriter.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunVrf.UtilHelpers
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    namespace LSystemsMG.Util.External
+    {
+        public class FileWriter : IDisposable
+        {
+            private StreamWriter sw;
+            private bool writeAsHtml = false;
+            private bool swOpen = true;
+
+            private static string DEFAULT_DIR = @"Z:\dev\www\vcs.codecreation.dev\GEN-output2\";
+
+            public FileWriter(
+                string outputFilenamepath,
+                bool writeAsHtml = false,
+                bool useDefaultDir = false,
+                string htmlTitle = "",
+                string htmlHeader = ""
+            )
+            {
+                if (useDefaultDir)
+                {
+                    outputFilenamepath = $"{DEFAULT_DIR}{outputFilenamepath}";
+                }
+
+                this.writeAsHtml = writeAsHtml;
+                Debug.WriteLine($"Writing to {outputFilenamepath}");
+                sw = new StreamWriter(outputFilenamepath);
+                if (writeAsHtml)
+                {
+                    if (htmlHeader.Length == 0)
+                    {
+                        string filename = Path.GetFileName(outputFilenamepath);
+                        WriteHtmlHeader(filename, filename);
+                    } else
+                    {
+                        WriteHtmlHeader(htmlTitle, htmlHeader);
+                    }
+
+                }
+                sw.NewLine = "\n";
+            }
+
+            public void CloseStreamWriter()
+            {
+                if (writeAsHtml)
+                {
+                    WriteHtmlFooter();
+                }
+                sw.Close();
+                swOpen = false;
+            }
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposing && sw != null)
+                {
+                    if (swOpen)
+                    {
+                        CloseStreamWriter();
+                    }
+                    sw.Dispose();
+                    sw = null;
+                }
+            }
+
+            public void Write(string text)
+            {
+                sw.Write(text);
+            }
+
+            public void WriteLine(string text)
+            {
+                sw.WriteLine(text);
+            }
+
+            public void WriteHtmlHeader(string browserTitle, string pageHeader)
+            {
+                writeAsHtml = true;
+                string html_header = "" +
+                    $"<!DOCTYPE html>\n<html>\n<head>\n  <title>{browserTitle}</title>\n" +
+                    $"  <link href='/includes/styles.css' rel='stylesheet' type='text/css' />\n" +
+                    $"</head>\n<body>\n<b>{pageHeader}</b>\n<pre>";
+                sw.WriteLine(html_header);
+            }
+
+            private void WriteHtmlFooter()
+            {
+                sw.WriteLine("</pre>\n</html>");
+            }
+        }
+    }
+}
+
